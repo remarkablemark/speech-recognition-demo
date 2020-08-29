ec8b4e97c9a0d6febbccd403e24329322c12b1fe
Mark <mark@remarkablemark.org>
2020-08-29 17:58:42 -0400

    feat(transcribe): add CLI option to save transcription to file

--- transcribe.py
+++ transcribe.py
@@ -1,15 +1,31 @@
 import speech_recognition as sr
 import sys
 
+# audio filepath
 try:
     audio_file = sys.argv[1]
 except:
     print('Missing audio file')
 
+# transcription filepath (optional)
+text_file = ''
+try:
+    text_file = sys.argv[2]
+except:
+    pass
+
+# transcribe audio to text
 r = sr.Recognizer()
 
 with sr.AudioFile(audio_file) as source:
     audio = r.listen(source)
 
 text = r.recognize_google(audio)
+
+# save transcription (if applicable)
+if text_file:
+    text_file = open(text_file, 'w')
+    text_file.write(text)
+
+# print transcription
 print(text)
