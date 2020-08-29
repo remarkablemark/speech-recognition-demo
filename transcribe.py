d2ab58ba64f6339e812779fa6b3b979fe3f2a32d
Mark <mark@remarkablemark.org>
2020-08-29 17:48:07 -0400

    feat(transcribe): pass audio filename as CLI option

--- transcribe.py
+++ transcribe.py
@@ -1,8 +1,14 @@
 import speech_recognition as sr
+import sys
+
+try:
+    audio_file = sys.argv[1]
+except:
+    print('Missing audio file')
 
 r = sr.Recognizer()
 
-with sr.AudioFile('audio.wav') as source:
+with sr.AudioFile(audio_file) as source:
     audio = r.listen(source)
 
 text = r.recognize_google(audio)
